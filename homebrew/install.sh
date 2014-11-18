9b005babe95423867117e67b7be88829c0bbb638
Hunter Matthews <thm@amazon.com>
2014-11-17 22:51:41 -0800

    Fixed my brew stuff due to bundle going away. Added tmux

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,21 +1,43 @@
-#! /usr/bin/env bash
+#! /usr/bin/env zsh
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-# Brew/homebrew is a mac only thing - don't run this on linux
-if [ "$(uname -s)" == "Darwin" ]
-then
-
-    # Install brew itself
-    if test ! $(which brew) ; then
-        echo "installing brew"
-        ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" > /tmp/brew-install.log
-    fi
-
-    # install (or update) all the things we get from Brew
-    brew bundle Brewfile
-
+# Brew/homebrew is a mac only thing - don't run this elsewhere
+if [[ "$(uname -s)" != "Darwin" ]] ; then
+    exit 1
 fi
 
-exit 0
+# Install brew itself
+if ! which brew >/dev/null ; then
+    echo "installing brew"
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)" | tee /tmp/brew-install.log
+fi
+
+# Make sure we’re using the latest Homebrew and formulas
+brew update
+brew upgrade
+
+brew install --cocoa --srgb emacs
+brew install tmux 
+brew install tree
+brew install figlet
+
+
+# Keep chickening out on these
+#brew install caskroom/cask/brew-cask
+#brew tap caskroom/versions
+
+# brew cask install dropbox 2> /dev/null
+# brew cask install google-chrome 2> /dev/null
+# brew cask install iterm2 2> /dev/null
+# brew cask install the-unarchiver 2> /dev/null
+# brew cask install transmission 2> /dev/null
+# brew cask install virtualbox 2> /dev/null
+# brew cask install vlc 2> /dev/null
+
+# Remove outdated versions from the cellar
+brew cleanup
+
+## END OF LINE ##
+
