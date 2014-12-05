380c33b06520b45375c28dbf3076f8c8a03dd5d3
Hunter Matthews <thm@amazon.com>
2014-12-04 19:43:54 -0800

    add an alias to remove comments from a file/stream

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,4 +6,7 @@ alias printenv="/usr/bin/env  | sort"
 # I prefer the old sort order -capitals at the top, then everything else. Mac/BSD goes this. Fake it
 alias ll='LANG=C ls -lh'
 
+# Remove commnets and whitespace only /blank lines from input (to be used in a pipeline)
+alias nocomment="sed -e 's/#.*$//' -e '/^\s*$/d'"
+
 ## END OF LINE ##
