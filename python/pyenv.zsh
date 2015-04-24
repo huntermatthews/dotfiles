55df718a5aef4559091a234664ca0a272a88ae07
Hunter Matthews <thm@amazon.com>
2015-04-23 19:54:40 -0700

    update old journal macros, enable pyenv on more than just laptop

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,12 +1,17 @@
 # -*- mode: sh -*-
 
-if [[ $DISPLAY_HOSTNAME != "digger" ]] ; then
-    return
+#if [[ $DISPLAY_HOSTNAME != "digger" ]] ; then
+#    return
+#fi
+
+
+if which pyenv > /dev/null; then
+    export PYENV_ROOT=/usr/local/opt/pyenv
+    eval "$(pyenv init -)"
 fi
 
-export PYENV_ROOT=/usr/local/opt/pyenv
-
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+if which pyenv-virtualenv-init > /dev/null; then
+    eval "$(pyenv virtualenv-init -)"
+fi
 
 ## END OF LINE ##
