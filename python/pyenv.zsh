1f69c6af2ca16d2e7ebd2a05ae25c0a3e08cad4d
Hunter Matthews <thm@amazon.com>
2015-05-01 14:43:46 -0700

    fix different which behavior old rhel vs mac

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,16 +1,11 @@
 # -*- mode: sh -*-
 
-#if [[ $DISPLAY_HOSTNAME != "digger" ]] ; then
-#    return
-#fi
-
-
-if which pyenv > /dev/null; then
+if which pyenv > /dev/null 2>&1 ; then
     export PYENV_ROOT=/usr/local/opt/pyenv
     eval "$(pyenv init -)"
 fi
 
-if which pyenv-virtualenv-init > /dev/null; then
+if which pyenv-virtualenv-init > /dev/null 2>&1 ; then
     eval "$(pyenv virtualenv-init -)"
 fi
 
