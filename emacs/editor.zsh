3cd8f4a0374e4484b90b58290c473d0ae117aa43
Hunter Matthews <hunter@pobox.com>
2015-08-19 17:15:39 -0700

    no idea

--- emacs/editor.zsh
+++ emacs/editor.zsh
@@ -1,7 +1,7 @@
 export EDITOR=e
 export VISUAL=$EDITOR
 
-source ~/.localrc
+[ -f ~/.localrc ] && source ~/.localrc
 
 # I know this occluds /usr/bin/view - thats vim, Im an emacs weenie
 view() { e --eval "(view-file \"$1\")" }
