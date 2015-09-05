2bd167c91c7a4346559f0e2e1499c5e641d4dcec
Hunter Matthews <thm@fb.com>
2015-09-04 19:25:01 -0700

    various emacs updates

--- emacs/work.el
+++ emacs/work.el
@@ -1,2 +1,18 @@
 (defvar master-dir (getenv "ADMIN_SCRIPTS"))
-(load-library (concat master-dir "/master.emacs"))
+(defvar master-emacs (concat master-dir "/master.emacs"))
+(if (file-exists-p master-emacs)
+    (load-library master-emacs))
+
+;; Now undue the sillyness
+
+;; This breaks tmux.conf amongst other things
+(remove-hook 'write-file-hooks 'delete-trailing-whitespace-sometimes)
+(remove-hook 'write-file-hooks 'delete-trailing-whitespace)
+
+;; No, I don't want to see it either
+(setq-default highlight-80+-columns 120)
+(setq-default show-trailing-whitespace nil)
+(setq-default highlight-80+-mode nil)
+
+
+;; END OF LINE ;;
