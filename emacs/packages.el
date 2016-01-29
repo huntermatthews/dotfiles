3528f93f03f2bb906bfe6ad5bac7fa5674f1249a
Hunter Matthews <thm@fb.com>
2016-01-29 11:43:30 -0800

    Updated emacs config to avoid having to name all files to load and avoid shadowing of std modules

--- emacs/packages.el
+++ emacs/packages.el
@@ -1,4 +1,4 @@
-(when (string= (getenv "NO_OUTSIDE_NET") "FALSE")
+;(when (string= (getenv "NO_OUTSIDE_NET") "FALSE")
 
 ;; Pull from stable?
 (require 'package)
@@ -9,17 +9,17 @@
 ;;(defvar my-packages '(starter-kit zenburn-theme auctex color-theme-solarized csharp-mode ecb_snap find-file-in-project flymake-css flymake-php idle-highlight ido-ubiquitous ipython python-mode magit markdown-mode paredit pastels-on-dark-theme php-mode rainbow-mode smex solarized-theme starter-kit-js zenburn-theme)
 ;;  "A list of packages to ensure are installed at launch.")
 
-(setq package-list '(rust-mode))
-
 ; fetch the list of packages available 
 (unless package-archive-contents
   (package-refresh-contents))
+
+(setq package-list '(rust-mode yaml-mode))
 
 ; install the missing packages
 (dolist (package package-list)
   (unless (package-installed-p package)
     (package-install package)))
 
-) ;; (when (string= (getenv "EMACS_DISABLE_ELPA") "FALSE")
+;) ;; (when (string= (getenv "EMACS_DISABLE_ELPA") "FALSE")
 
 ;; END OF LINE ;;
