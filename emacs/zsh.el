759dffa57ecfab41567f5c7fdd0900eff9c4b305
Hunter Matthews <thm@amazon.com>
2017-01-09 22:36:59 -0500

    checkin old changes

--- emacs/zsh.el
+++ emacs/zsh.el
@@ -2,4 +2,5 @@
 (add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
 (add-to-list 'auto-mode-alist '("\\.zshenv\\'" . sh-mode))
 
+;(add-hook 'sh-mode-hook (lambda () (setq tab-width 0 sh-basic-offset 4 indent-tabs-mode nil)))
 ;; END OF LINE ;;
