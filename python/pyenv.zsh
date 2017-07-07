557ea68f4e54d475f6a5cd7d19accb9ab54bd5de
Hunter Matthews <hunter@pobox.com>
2017-07-07 10:56:45 -0400

    minor differences everywhere

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,6 +1,7 @@
 # -*- mode: sh -*-
 
-VIRTUAL_ENV_DISABLE_PROMPT=1
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
 if which pyenv > /dev/null 2>&1 ; then
     export PYENV_ROOT=/usr/local/opt/pyenv
