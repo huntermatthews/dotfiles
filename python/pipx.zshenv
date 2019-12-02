17e616455588674987d073c2716fd0452a378ec5
Hunter Matthews <hunter.matthews@nih.gov>
2019-12-02 14:22:52 -0500

    Cleanup duplicate pyenv config so I loose my mind less

---
+++ python/pipx.zshenv
@@ -0,0 +1,5 @@
+## Add all the osx specific path locations
+
+path+=(~/.local/bin)
+
+## END OF LINE ##
--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -2,17 +2,23 @@
 
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+export PYENV_ROOT=/usr/local/opt/pyenv
 
 if which pyenv > /dev/null 2>&1 ; then
-    export PYENV_ROOT=/usr/local/pyenv
     eval "$(pyenv init -)"
+    #export PATH=${pyenvdir}/bin:$PATH
+    function pyenv_prompt_info() {
+        echo "[py:$(pyenv version-name)] "   # gah I hate that space
+    }
+else
+    function pyenv_prompt_info() { 
+        echo "[python: $(python -V 2>&1 | cut -f 2 -d ' ')] " # gah I hate that space
+    }
 fi
 
 if which pyenv-virtualenv-init > /dev/null 2>&1 ; then
     eval "$(pyenv virtualenv-init -)"
 fi
 
-#function pyenv_prompt_info() {
-#            echo "$(pyenv version-name)"
-#        }
+
 ## END OF LINE ##
