c21cc92bdd5afa1e9667fb9a53ed46a9ec6a4df6
Hunter Matthews <hunter.matthews@nih.gov>
2022-12-07 19:41:20 -0500

    Improve/simplify python virtual env handling in prompt

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,19 +1,20 @@
 # -*- mode: sh -*-
 
-export VIRTUAL_ENV_DISABLE_PROMPT=1
 export PYENV_ROOT=~/.local/pyenv
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 if which pyenv > /dev/null 2>&1 ; then
     eval "$(pyenv init -)"
     export PATH=$(pyenv root)/shims:$PATH
-    function pyenv_prompt_info() {
-        echo "[py:$(pyenv version-name)] "   # gah I hate that space
-    }
-else
-    function pyenv_prompt_info() { 
-        echo "[python: $(python -V 2>&1 | cut -f 2 -d ' ')] " # gah I hate that space
-    }
 fi
+
+function pyenv_prompt_info() {
+    if [[ -v VIRTUAL_ENV ]] ; then
+        echo "[venv3: ${$(python3 -V)#Python }] "
+    else
+        echo "[py3: ${$(python3 -V)#Python }] "   # gah I hate that space
+    fi
+}
 
 ## Causing more trouble than it was worth with poetry.
 # Switch to direnv for equivalent and generic support.
