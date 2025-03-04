473c14e10a0385b24600d965a10280b263366cc8
Hunter Matthews <hunter.matthews@nih.gov>
2025-03-04 14:26:37 -0500

    Happily, citrix is no longer a thing

--- work/remove_citrix.sh
+++
@@ -1,1 +0,0 @@
-sudo rm -rf /Library/Internet\ Plug-Ins/CitrixICAClientPlugIn.plugin /Library/LaunchAgents/com.citrix.* /Users/Shared/Citrix /Users/Shared/Citrix\ Receiver ~/Library/Application\ Support/Citrix\ Receiver/ ~/Library/Preferences/com.citrix.* /private/var/db/receipts/com.citrix.*
