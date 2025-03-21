842726a32515c82c442f424e776dead7109d0f84
Hunter Matthews <hunter.matthews@nih.gov>
2025-03-21 18:21:36 -0400

    macos_certs_updates is its own file
    Should this go to libexec? Probably....

--- python/python.fish
+++ python/python.fish
@@ -1,14 +1,5 @@
 ## -*- mode: fish -*- ##
 
-# This  should be unnecessary now that truststore is a thing.
-if [ -r /etc/ssl/cacerts.pem ]
-    # For requests library until 3.0 ships
-    set -gx REQUESTS_CA_BUNDLE /etc/ssl/cacerts.pem
-
-    # For regular python urllib <sigh>
-    set -gx SSL_CERT_FILE /etc/ssl/cacerts.pem
-end
-
 # Prevent pip and sometimes pyenv from making a mess of things.
 # If I need it globably, I have to unset the variable.
 set -gx PIP_REQUIRE_VIRTUALENV true
