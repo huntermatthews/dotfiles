2213ebd65424e3ac3fd65c18d872e461662e4d53
Hunter Matthews <hunter.matthews@nih.gov>
2025-03-25 18:04:50 -0400

    This is really an MacOS ism

--- osx/osx.fish
+++ osx/osx.fish
@@ -3,4 +3,10 @@
 ## Finger memory says its md5sum
 alias md5sum 'md5 -r'
 
+# make unfortunately is installed as gmake. Because of $TEAM, I need it to be "make"
+# and it can't be an alias.... dangit.
+if brew list make 1>/dev/null 2>&1
+    fish_add_path --global --move --path /opt/homebrew/opt/make/libexec/gnubin
+end
+
 ## EOF ##
