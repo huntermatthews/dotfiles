81833d926e4f3323e5415fa250c5184864ff2473
Hunter Matthews <hunter@pobox.com>
2025-03-27 00:08:03 -0400

    Move the git scripts into libexec and update alias

--- git/git.fish
+++ git/git.fish
@@ -3,6 +3,6 @@
 # Make an alias to add the .dotfiles/git directory to the path ONLY for the git command.
 # This will do what we want with git, and clean out a bunch of junk from bin.
 
-alias git 'PATH=$HOME/.dotfiles/git:$PATH command git'
+alias git 'PATH=$DOTS/libexec/git:$PATH command git'
 
 ## END OF LINE ##
