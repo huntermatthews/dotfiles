d400b33243540d78f60e31dc65f44aa5100d2c39
Hunter Matthews <hunter@unix.haus>
2025-03-28 13:15:04 -0400

    Remove remaining zsh configs from old setup.

--- direnv/direnv.zsh
+++
@@ -1,6 +0,0 @@
-# Don't continue if direnv is not found
-command -v direnv &>/dev/null || return
-
-eval "$(direnv hook zsh)"
-
-## END OF LINE ##
--- work_profile/path.zshenv
+++
@@ -1,7 +0,0 @@
-# -*- mode: sh -*-
-
-
-# on linux (at least my dev machine)
-path+=(/usr/local/adm/bin)
-
-## END OF LINE ##
