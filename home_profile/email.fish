8ba96b9d2e30c3a07349483199aa1b7b42eb91e8
Hunter Matthews <hunter@unix.haus>
2025-03-28 13:16:48 -0400

    Remove now useless comments.

--- home_profile/email.fish
+++ home_profile/email.fish
@@ -1,6 +1,5 @@
 ## -*- mode: fish -*- ##
 
-# This should be different away from work...
 set -gx EMAIL "hunter@unix.haus"
 set -gx ENAME "Hunter Matthews"
 
--- work_profile/email.fish
+++ work_profile/email.fish
@@ -1,6 +1,5 @@
 ## -*- mode: fish -*- ##
 
-# This should be different away from work...
 set -gx EMAIL "hunter.matthews@nih.gov"
 set -gx ENAME "Hunter Matthews"
 
