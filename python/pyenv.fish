c66017733d1d84f3347e90e894762447592fccf7
Hunter Matthews <hunter@unix.haus>
2025-03-31 20:50:02 -0400

    update pyenv to correct fish syntax and macos config

--- python/pyenv.fish
+++ python/pyenv.fish
@@ -1,20 +1,11 @@
 ## -*- mode: fish -*- ##
 
-export PYENV_ROOT=~/.local/pyenv
-export VIRTUAL_ENV_DISABLE_PROMPT=1
+set -gx PYENV_ROOT ~/.local/pyenv
+set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
 
 if command -v pyenv >/dev/null 2>&1
     pyenv init - fish | source
 end
-
-
-# function pyenv_prompt_info() {
-#     if [[ -v VIRTUAL_ENV ]] ; then
-#         echo "[**VENV**: ${$(python3 -V)#Python }] "
-#     else
-#         echo "[py: ${$(python3 -V)#Python }] "   # gah I hate that space
-#     fi
-# }
 
 
 ## END OF LINE ##
