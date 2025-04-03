84cabee1674185b1a21faa3bc33657afc9c5480c
Matthews <matthewsht@hg-02254242-lm.nih.gov>
2025-04-03 19:04:03 -0400

    Improve dots in various ways.
    I do like fish...

--- libexec/dots/common-lib.fish
+++ libexec/dots/common-lib.fish
@@ -1,8 +1,10 @@
 # -*- mode: fish -*-
 
 # This applies to DOTS as a whole - not just one script - so we set it in the common library.
-set -g DOTS_VERSION "0.10"
+set -g DOTS_VERSION "0.80"
+set -g DOTS_COMMON_REQUIRED_PROGRAMS
 
+# FIXME: This should NOT panic, but return false
 function dots_require_universal_variables
     for var in $argv
         if not set -q -U $var
@@ -11,6 +13,7 @@ function dots_require_universal_variables
     end
 end
 
+# FIXME: This should NOT panic, but return false
 function dots_require_directories
     for var in $argv
         if not test -d $var
@@ -19,14 +22,37 @@ function dots_require_directories
     end
 end
 
+function dots_require_programs
+    # this is just command -v, BUT I want to be able to check for multiple programs at once.
+    for prog in $argv
+        if not command -v $prog
+            return 1
+        end
+    end
+    return 0
+end
+
+function dots_require_profile_to_be_valid
+    # here, 'valid' means that the dir exists.
+    if contains $DOTS_PROFILE (dots_profile_list)
+        return 0
+    else
+        return 1
+    end
+end
+
 function dots_require_base_config
     # Require that certain universal variables exist
     dots_require_universal_variables DOTS DOTS_LOCAL DOTS_PROFILE
 
-    # Require certain directories exist
-    dots_require_directories $DOTS $DOTS_LOCAL $DOTS/$DOTS_PROFILE_profile $DOTS/libexec/dots
+    # Require our DOTS_PROFILE to be in the list of possible profiles. (IE, dir that exists)
+    dots_require_profile_to_be_valid
 
-    # TODO: Make sure that the profile is set to an actual _profile dir in $DOTS
+    # Require certain directories exist
+    # NOTE: we don't test the profile dir again here because we did that above.
+    dots_require_directories $DOTS $DOTS_LOCAL $DOTS/libexec/dots
+
+    dots_require_programs $DOTS_COMMON_REQUIRED_PROGRAMS
 
 end
 
@@ -68,7 +94,7 @@ end
 
 function dots_files_with_active_profile
 
-    # We need 1 positional arguments = the pattern to match
+    # We need 1 positional argument = the pattern to match
     if test (count $argv) -ne 1
         panic "missing required arguments"
     end
@@ -77,6 +103,7 @@ function dots_files_with_active_profile
     debug --var pattern
 
     # Get ALL the pattern matched files - including ALL the profiles
+    # We DON"T search deeply - by design so far.
     set files $DOTS/*/*$pattern
     debug --var-spaces files
 
@@ -89,29 +116,64 @@ function dots_files_with_active_profile
     debug --var-spaces profile_files
 
     # and add those profile files back into the main list
-    # BUGFIX: put profile at end to allow for overrides.
+    # FIXED: put profile at end to allow for overrides.
     set files $files $profile_files
     debug --var-spaces files
 
     echo $files
 end
 
+# For all of dots, this is correct - my code, my rules.
 function fish_command_not_found
     exit 12
+end
+
+# from https://fishshell.com/docs/current/cmds/fish_git_prompt.html:
+# "Boolean options (those which enable or disable something) understand “1”, “yes” or “true” to
+# mean true and every other value to mean false." Should I add "True" to this list?
+# Recall that for SHELL/fish programming, an 'if' test is reversed from normal boolean logic.
+#      Think of it like this - did X fail? if X == 'did X fail?' -- which cause the true block to be '0'
+#      Yes, shell programming is weird - its not my fault.
+function is_true
+    set truths true yes 1
+    if contains $arg $truths
+        return 0
+    end
+    return 1
+end
+function is_false
+    set truths true yes 1
+    if contains $arg $truths
+        return 1
+    end
+    return 0
+end
+function is_false_or_empty
+    # This is a bit of a hack - but it works.
+    # We want to check for empty or false - so we check for empty first.
+    # If its empty, we return success(0). If its not empty, we check for false.
+    # If its false, we return success. If its not false, we return failure.
+    if test -z $arg
+        return 0
+    end
+    if is_false $arg
+        return 0
+    end
+    return 1
+end
+
+function dots_profile_list
+    echo path filter --type dir $DOTS/libexec/dots/*_profile | string _profile ''
 end
 
 ##
 ## Future
 ##
 
-# TODO: write an is_true/is_false that allows 1/0 true/false True/False etc
-# from https://fishshell.com/docs/current/cmds/fish_git_prompt.html:
-# "Boolean options (those which enable or disable something) understand “1”, “yes” or “true” to
-# mean true and every other value to mean false."
-
-# TODO: dots_require_programs - check for the existence of programs in $PATH
-# isn't that just command -v? Yes, but I want to be able to check for multiple programs at once.
-
-# TODO: dots_profile_list - look for correct pattern and return names
+# TODO: rewrite the existing debug to be:
+# debug --var might be fine
+# debug_var ???
+# format_spaces
+#    but definitely get spaces OUT of the debug function. too complex
 
 ## END OF LINE ##
