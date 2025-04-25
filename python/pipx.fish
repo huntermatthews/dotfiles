d9fe89e52927d03a1b71cd7c7786b507bdbd52bf
Hunter Matthews <hunter.matthews@nih.gov>
2025-04-25 17:52:32 -0400

    Fix dumb oversight

--- python/pipx.fish
+++ python/pipx.fish
@@ -1,6 +1,6 @@
 ## Add all the osx specific path locations
 
-if command -v pipx
+if command -v pipx >/dev/null
     # PipX installs everything in ~/.local/bin
     fish_add_path --global --move --path ~/.local/bin
 end
