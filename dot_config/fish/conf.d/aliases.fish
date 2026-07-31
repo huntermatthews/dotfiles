80dc2c7c04ce66a9a8709e6d4b22ae0df940deaf
Hunter Matthews <hunter.matthews@nih.gov>
2026-07-31 16:48:35 -0500

    minor update

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -1,46 +1,47 @@
 ###########################
 # ALIASES
 ###########################
-## Finger memory says its md5sum
-alias md5sum 'md5 -r'
 
-# make unfortunately is installed as gmake.
-# I think it can just be an alias and we're fine -- 2025-03-28
-alias make gmake
-
+alias cz 'chezmoi'
 
 alias diff 'diff -u'
+
+alias ee 'zed -n'
+alias e 'zed'
 
 # Show the thing I want anyways
 alias forks "fork status"
 
-# I prefer the old sort order -capitals at the top, then everything else. Mac/BSD goes this. Fake it
+# I prefer the old sort order -capitals at the top, then everything else. Mac/BSD does this. Fake it
 alias ll 'LANG=C ls -lhH'
 alias la 'LANG=C ls -lahH'
 
-alias nless "less -N"
+# homebrew make unfortunately is installed as gmake.
+# I think it can just be an alias and we're fine -- 2025-03-28
+alias make gmake
 
-alias rfcdate 'date +%FT%T%z'
+alias md5sum 'md5 -r'
+
+alias nless "less -N"
 
 # printenv does something stupid in some places - I forgot what or where
 # Why is the output of neither printenv or env sorted by default?
 alias printenv "/usr/bin/env | sort"
 
-# Remove comments and whitespace only /blank lines from input (to be used in a pipeline)
-# alias nocomment 'sed -e "s/#.*\$//" -e "/^\s*\$/d"'
-# alias wsstrip 'sed -e "s/^[ \t]*//" -e "s/[ \t]*\$//" -e "s/[ \t][ \t]*/ /"'
-
 # Create a randomized password good enough to actually use.
 alias pwgen 'pwgen --secure 30 1'
+
+alias rfcdate 'date +%FT%T%z'
 
 # this formats shell scripts (bash / zsh) "correctly"
 # fish uses fish_indent.
 alias shfmt 'shfmt -i 4 -bn -ci -sr'
 
 # terraform is too much to spell
-alias tf $HOME/code/cloud/ssa-terraform/bin/ssa-terraform
+alias tf $HOME/code/cloud/ssa-terraform/main/bin/ssa-terraform
 
 alias tsize 'echo $LINES x $COLUMNS'
 
-alias vs 'code --new-window '
-alias ee vs
+# Remove comments and whitespace only /blank lines from input (to be used in a pipeline)
+# alias nocomment 'sed -e "s/#.*\$//" -e "/^\s*\$/d"'
+# alias wsstrip 'sed -e "s/^[ \t]*//" -e "s/[ \t]*\$//" -e "s/[ \t][ \t]*/ /"'
